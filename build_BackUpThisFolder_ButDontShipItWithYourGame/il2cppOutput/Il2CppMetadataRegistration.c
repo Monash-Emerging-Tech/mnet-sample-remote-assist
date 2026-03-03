38d50d0a1ef3ecc89d1ff8131c75e1a62c648859
jaynav1 <me@jyln.au>
2026-03-03 17:29:53 +1100

    Add passthrough samples webrtc and build

---
+++ build_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	16889,
+	g_Il2CppGenericTypes,
+	12144,
+	g_Il2CppGenericInstTable,
+	160144,
+	g_Il2CppGenericMethodFunctions,
+	53527,
+	g_Il2CppTypeTable,
+	184990,
+	g_Il2CppMethodSpecTable,
+	15809,
+	g_FieldOffsetTable,
+	15809,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
